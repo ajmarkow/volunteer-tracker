de32e788873a3144e962e26e7b20122753759cab
AJ Markow <alexanderjmarkow@gmail.com>
2020-10-09 14:56:42 -0700

    Pass 2/4 integration tests.  Add project title and edit project views.

--- app.rb
+++ app.rb
@@ -8,3 +8,26 @@ require("pg")
 require "dotenv/load"
 
 DB = PG.connect({ :dbname => "volunteer_tracker", :user => "postgres", :password => ENV["PG_PASS"] })
+
+get("/") do
+  @projects = Project.all
+  @volunteer = Volunteer.all
+  erb(:homepage)
+end
+
+get("/project/:id") do
+  @project = Project.find(:id)
+  erb(:project)
+end
+
+get("/editproject/:id") do
+  @project = Project.find(:id)
+  erb(:editproject)
+end
+
+post("/create_project") do
+  entered_title = params[:title]
+  newproject = Project.new({ :title => entered_title, :id => nil })
+  newproject.save()
+  redirect to ("/")
+end
