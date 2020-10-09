ab1a1df8b795d27f3bd1bae4175717d121b195ff
AJ Markow <alexanderjmarkow@gmail.com>
2020-10-09 09:10:38 -0700

    Add gemfile.lock. Update business logic file names.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,11 +4,11 @@ require "rspec"
 require "pry"
 require "pg"
 
-DB = PG.connect({:dbname => 'volunteer_tracker_test'})
+DB = PG.connect({ :dbname => "volunteer_tracker_test", :password => ENV["PGPASS"] })
 
 RSpec.configure do |config|
   config.after(:each) do
-    DB.exec('DELETE FROM volunteers *;')
-    DB.exec('DELETE FROM projects *;')
+    DB.exec("DELETE FROM volunteers *;")
+    DB.exec("DELETE FROM projects *;")
   end
 end
