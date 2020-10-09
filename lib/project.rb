0ffdae65b32bf2cf84679caac7034a8782afc06f
AJ Markow <alexanderjmarkow@gmail.com>
2020-10-09 10:55:29 -0700

    Update business logic files to add .all methods for Project and Volunteer classes.

--- lib/project.rb
+++ lib/project.rb
@@ -6,4 +6,24 @@ class Project
     @title = attributes.fetch(:title)
     @id = attributes.fetch(:id)
   end
+
+  def ==(project_to_compare)
+    self.title() == project_to_compare.title()
+  end
+
+  def save
+    entry = DB.exec("INSERT INTO projects (title) values ('#{@title}') RETURNING id")
+    @id = entry.first().fetch("id").to_i
+  end
+
+  def self.all
+    all_projects = DB.exec("SELECT * FROM projects")
+    projects = []
+    all_projects.each() do |entry|
+      title = entry.fetch("title")
+      id = entry.fetch("id").to_i
+      projects.push(Project.new({ :title => title, :id => id }))
+    end
+    projects
+  end
 end
--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -1,7 +1,31 @@
 class Volunteer
+  attr_accessor(:name, :project_id)
+  attr_reader(:id)
+
   def initialize(attributes)
     @name = attributes.fetch(:name)
     @id = attributes.fetch (:id)
     @project_id = attributes.fetch(:project_id)
+  end
+
+  def ==(volunteer_to_compare)
+    self.name() == volunteer_to_compare.name()
+  end
+
+  def save
+    entry = DB.exec("INSERT INTO volunteers (name) values ('#{@name}') RETURNING id")
+    @id = entry.first().fetch("id").to_i
+  end
+
+  def self.all
+    all_volunteers = DB.exec("SELECT * FROM volunteers")
+    volunteers = []
+    all_volunteers.each() do |entry|
+      name = entry.fetch("name")
+      project_id = entry.fetch("project_id")
+      id = entry.fetch("id").to_i
+      volunteers.push(Volunteer.new({ :name => name, :project_id => project_id, :id => id }))
+    end
+    volunteers
   end
 end
